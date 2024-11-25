48cc0bcf5197140171f25ac03b99e16e4a653016
Kevin <kevinknoel7@gmail.com>
2024-11-25 08:13:02 +0530

    minor projects section refactor

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import { motion, MotionConfig, useInView, Variants } from "framer-motion";
+import { motion, useInView, Variants } from "framer-motion";
 import { useEffect, useRef } from "react";
 import diagonalArrow from "../assets/diagonal-arrow-icon.svg";
 import { useActiveLink } from "../hooks/useActiveLink";
@@ -33,6 +33,11 @@ const projectVariants: Variants = {
   show: {
     opacity: 1,
     x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      damping: 20,
+    },
   },
 };
 
@@ -51,62 +56,53 @@ export default function Projects() {
   return (
     <Container ref={projectsRef} id="projects">
       <div className="flex flex-col justify-center gap-8 mt-5 mb-20 w-full min-h-[inherit]">
-        <MotionConfig
-          transition={{
-            type: "spring",
-            stiffness: 80,
-            damping: 20,
-          }}
+        <motion.h2
+          className="text-2xl font-bold ml-2"
+          custom={0}
+          variants={projectVariants}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
         >
-          <motion.h2
-            className="text-2xl font-bold ml-2"
-            custom={0}
-            variants={projectVariants}
-            initial="hidden"
-            whileInView="show"
-            viewport={{ once: true }}
-          >
-            My Projects:
-          </motion.h2>
+          My Projects:
+        </motion.h2>
 
-          <div className="grid grid-cols-1 w-full gap-4">
-            {projects.map((project, i) => {
-              return (
-                <HoverCard
-                  key={project.title}
-                  custom={i + 1}
-                  variants={projectVariants}
-                  initial="hidden"
-                  whileInView="show"
-                  viewport={{ once: true }}
-                >
-                  <div className="flex flex-col justify-between w-full min-h-52 gap-4 p-4">
-                    <h2 className="text-xl font-bold">{project.title}</h2>
+        <div className="grid grid-cols-1 w-full gap-4">
+          {projects.map((project, i) => {
+            return (
+              <HoverCard
+                key={project.title}
+                custom={i + 1}
+                variants={projectVariants}
+                initial="hidden"
+                whileTap={{ scale: 0.95 }}
+                whileInView="show"
+                viewport={{ once: true }}
+              >
+                <div className="flex flex-col justify-between w-full min-h-52 gap-4 p-4">
+                  <h2 className="text-xl font-bold">{project.title}</h2>
 
-                    <p className="w-full max-w-[800px]">
-                      {project.description}
-                    </p>
+                  <p className="w-full max-w-[800px]">{project.description}</p>
 
-                    <a
-                      href={project.url}
-                      target="_blank"
-                      className="text-lg cursor-pointer"
-                    >
-                      Visit{" "}
-                      <span className="inline-block opacity-0 -translate-x-5 group-hover:opacity-100 group-hover:translate-x-0 transition-[opacity_transform] duration-300">
-                        <img
-                          src={diagonalArrow}
-                          alt="diagonal arrow icon"
-                          className="size-3.5"
-                        />
-                      </span>
-                    </a>
-                  </div>
-                </HoverCard>
-              );
-            })}
-          </div>
-        </MotionConfig>
+                  <a
+                    href={project.url}
+                    target="_blank"
+                    className="text-lg cursor-pointer"
+                  >
+                    Visit{" "}
+                    <span className="inline-block opacity-0 -translate-x-5 group-hover:opacity-100 group-hover:translate-x-0 transition-[opacity_transform] duration-300">
+                      <img
+                        src={diagonalArrow}
+                        alt="diagonal arrow icon"
+                        className="size-3.5"
+                      />
+                    </span>
+                  </a>
+                </div>
+              </HoverCard>
+            );
+          })}
+        </div>
       </div>
     </Container>
   );
